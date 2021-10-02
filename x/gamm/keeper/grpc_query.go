e44c9a231a7b2690d1e32423a9d531093cf45808
chachean <chachean84@hotmail.com>
2021-10-02 23:19:05 +0800

    Update grpc_query.go

--- x/gamm/keeper/grpc_query.go
+++ x/gamm/keeper/grpc_query.go
@@ -81,7 +81,7 @@ func (k Keeper) Pools(
 	store := sdkCtx.KVStore(k.storeKey)
 	poolStore := prefix.NewStore(store, types.KeyPrefixPools)
 
-	poolIds := []uint64{1, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 22, 42, 183, 197, 481, 482} 
+	poolIds := []uint64{1, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 22, 42, 183, 197, 481, 482, 497, 498} 
 
 	var anys []*codectypes.Any
 	pageRes, err := query.Paginate(poolStore, req.Pagination, func(_, value []byte) error {
